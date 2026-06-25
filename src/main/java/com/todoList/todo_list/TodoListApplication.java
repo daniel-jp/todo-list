08f05cde1a2c72e05c7731a25d8845c4445bc8ce
daniel-jp <danny198672@hotmail.com>
2026-06-25 20:24:47 +0000

    update

--- src/main/java/com/todoList/todo_list/TodoListApplication.java
+++ src/main/java/com/todoList/todo_list/TodoListApplication.java
@@ -1,12 +1,22 @@
 package com.todoList.todo_list;
 
+import com.todoList.todo_list.entity.Role;
+import com.todoList.todo_list.entity.User;
+import com.todoList.todo_list.repositories.RoleRepository;
+import com.todoList.todo_list.repositories.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 @EnableScheduling
-public class TodoListApplication  {
+public class TodoListApplication implements CommandLineRunner {
 
 
     public static void main(String[] args) {
@@ -14,7 +24,6 @@ public class TodoListApplication  {
     }
 
 
-   /*implements CommandLineRunner
     @Override
     public void run(String... args) throws Exception {
 
@@ -76,8 +85,6 @@ public class TodoListApplication  {
 
         };
     }
-
-    */
 
 
 }
