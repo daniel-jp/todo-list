71803a8d5adac001b44f4abc4d5acba995135cb1
daniel-jp <danny198672@hotmail.com>
2026-06-29 03:19:59 +0000

    Teste deploy 1

--- Dockerfile
+++ Dockerfile
@@ -1,27 +1,18 @@
-# =========================
-# BUILD STAGE
-# =========================
 
-FROM maven:3.9.9-eclipse-temurin-21 AS build
-
-WORKDIR /app
-
-COPY pom.xml .
-
-COPY src ./src
-
-RUN mvn clean package -DskipTests
-
-# =========================
-# RUNTIME STAGE
-# =========================
-
-FROM eclipse-temurin:21-jre
+FROM eclipse-temurin:21-jdk
+VOLUME /tmp
+# Instala o curl para permitir que o Healthcheck do Docker funcione
+RUN apt-get update && \
+    apt-get install -y curl && \
+    rm -rf /var/lib/apt/lists/*
 
 WORKDIR /app
 
-COPY --from=build /app/target/*.jar app.jar
+COPY target/*.jar app.jar
 
 EXPOSE 8080
 
 ENTRYPOINT ["java","-jar","app.jar"]
+
+## In terminal, if we repect the filename as Dockerfile, we do'nt need to right for ex : ($ docker build . -t Dockerfile:v1) to create image.
+## with file name Dockerfile weclear can especify an ather name like ($ docker build . -t do-tasks:v1)
--- src/main/java/com/todoList/todo_list/TodoListApplication.java
+++ src/main/java/com/todoList/todo_list/TodoListApplication.java
@@ -1,91 +1,17 @@
 package com.todoList.todo_list;
 
-import com.todoList.todo_list.entity.Role;
-import com.todoList.todo_list.entity.User;
-import com.todoList.todo_list.repositories.RoleRepository;
-import com.todoList.todo_list.repositories.UserRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 @EnableScheduling
-public class TodoListApplication implements CommandLineRunner{
+public class TodoListApplication {
 
 
     public static void main(String[] args) {
         SpringApplication.run(TodoListApplication.class, args);
     }
-
-
-    @Override
-    public void run(String... args) throws Exception {
-
-    }
-
-
-    @Bean
-    public CommandLineRunner initAdmin(UserRepository userRepository,
-                                       RoleRepository roleRepository,
-                                       PasswordEncoder passwordEncoder) {
-        return args -> {
-
-            // 1 - Garantir que roles existam
-            Role adminRole = roleRepository.findByName("ROLE_ADMIN")
-                    .orElseGet(() -> roleRepository.save(new Role( "ROLE_ADMIN")));
-
-            Role userRole = roleRepository.findByName("ROLE_USER")
-                    .orElseGet(() -> roleRepository.save(new Role( "ROLE_USER")));
-
-            // 2 - Create admin user and user if it doesn't exist
-            User admin = userRepository.findByEmail("admin@system.com")
-                    .orElseGet(() -> {
-                        User u = new User();
-                        u.setEmail("admin@system.com");
-                        u.setName("Admin");
-                        u.setPassword(passwordEncoder.encode("admin12"));
-                        u.setEnabled(true);
-                        u.setLocked(false);
-                        return userRepository.save(u);
-                    });
-
-            User user = userRepository.findByEmail("user@system.com")
-                    .orElseGet(() -> {
-                        User u2 = new User();
-                        u2.setEmail("user@system.com");
-                        u2.setName("User");
-                        u2.setPassword(passwordEncoder.encode("user12"));
-                        u2.setEnabled(true);
-                        u2.setLocked(false);
-
-                        return userRepository.save(u2);
-                    });
-
-            //ensure the list is not null.
-
-            if (admin.getRoles() == null || user.getRoles()==null) {
-                admin.setRoles(new ArrayList<>());
-                user.setRoles(new ArrayList<>());
-            }
-
-            // 3 - Assign ADMIN role to admin user, if not already done
-            if ( !user.getRoles().contains(userRole)|| !admin.getRoles().contains(adminRole)) {
-
-                user.getRoles().add(userRole);
-                admin.getRoles().add(adminRole);
-                userRepository.saveAll(List.of(user,admin));
-                System.out.println("✅ ADMIN & USER role assigned to user");
-            }
-
-        };
-    }
-
 
 
 
